0054ef2cb2644b54608bf8b1a55620f6232e849e
bestoon <148741679+vestoon@users.noreply.github.com>
2024-03-19 15:39:47 +0900

    2023.03.28

---
+++ C++/15657.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+bool visited[100];
+int arr[100], num[100];
+int N, M;
+
+
+void dfs(int d, int id) {
+	if (d == M + 1) {
+		for (int i = 1; i < M + 1; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << endl;
+	}
+	else {
+		for (int i = id; i < N + 1; i++) {
+			/*if (visited[i] == true)
+				continue;*/
+				//visited[i] = true;
+			arr[d] = num[i];
+			dfs(d + 1, i);
+			//visited[i] = false;
+		}
+	}
+}
+
+
+int main() {
+	cin >> N >> M;
+
+	int x;
+	for (int i = 1; i < N + 1; i++) {
+		cin >> x;
+		num[i] = x;
+	}
+	sort(num + 1, num + N + 1);
+
+	/*cout << "num: ";
+	for (int i = 1; i < N + 1; i++) {
+		cout << num[i] << " ";
+	}
+	cout << endl;*/
+
+	dfs(1, 1);
+
+	return 0;
+}
