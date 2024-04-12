c907cebbb45d1a951adc10cc20d3b47ad4638305
bestoon <148741679+vestoon@users.noreply.github.com>
2024-04-12 20:19:53 +0900

    22.09.23

---
+++ Python/1764.py
@@ -0,0 +1,19 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+nothear = set()
+notsee = set()
+for h in range(N):
+    person = sys.stdin.readline().rstrip()
+    nothear.add(person)
+
+for s in range(M):
+    person = sys.stdin.readline().rstrip()
+    notsee.add(person)
+
+ans = sorted(nothear & notsee)
+print(len(ans))
+for x in ans:
+    print(x)
+
